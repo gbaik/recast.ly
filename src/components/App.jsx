806c9beb699ef64aedd2afb64931bb8abe515242
Hack Reactor Student <student@hackreactor.com>
2017-06-03 18:19:25 -0700

    Add interactions with Youtube API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -35,13 +35,3 @@ class App extends React.Component {
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
-
-//Initialize the state of App 
-//to keep track of all the videos in the video list and the current video in the player.
-//Pass this state down as props to its children components. 
-//Continue to use the example data.
-
-// Make it so that when the title of a VideoListEntry is clicked, that video is displayed in the player. 
-// Do not add state to any of the functional components.
-
-//this.onVideoListEntryClick.bind(this)
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,23 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      part: 'snippet',
+      key: options.key,
+      q: options.query,
+      maxResults: options.max,
+      videoEmbeddable: true,
+      type: 'video'
+    },
+    success: function (data) {
+      callback(data.items);
+      console.log('succesfully received data ', data);
+    }, 
+    error: function (data) {
+      console.error('failed to receive data ', data);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
